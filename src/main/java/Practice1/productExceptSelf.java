aeb3068c8de12414268d5c125b3d2d60d3c97a36
IshanGarg <officialgarg1@gmail.com>
2025-08-28 20:58:01 +0530

    added files

---
+++ src/main/java/Practice1/productExceptSelf.java
@@ -0,0 +1,38 @@
+package Practice1;
+
+/**
+ * Description:<br>
+ * Date: 28/08/25-8:22 pm
+ *
+ * @author ishangarg
+ * @since
+ */
+public class productExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        // Arrays to store prefix (left) and suffix (right) products
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] ans = new int[n];
+
+        // Left product
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        // Right product
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        // Final result: multiply left and right
+        for (int i = 0; i < n; i++) {
+            ans[i] = left[i] * right[i];
+        }
+
+        return ans;
+    }
+}
