f54d1a22e6fa46997925aea4655da130d32c8b4b
ishan.garg <officialgarg1@gmail.com>
2025-11-10 01:37:59 +0530

    done

--- src/main/java/Practice1/MinStack.java
+++ src/main/java/Practice1/MinStack.java
@@ -1,22 +1,20 @@
 package Practice1;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.NoSuchElementException;
-
+import java.util.Stack;
 
 public class MinStack {
-    // main stack holds all values
-    private final Deque<Integer> stack;
-    // minStack holds the current minima (one or more entries)
-    private final Deque<Integer> minStack;
+
+    private final Stack<Integer> stack;
+
+    private final Stack<Integer> minStack;
 
     public MinStack() {
-        stack = new ArrayDeque<>();
-        minStack = new ArrayDeque<>();
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
-    // push value onto stack; also push to minStack if it's new minimum
+
     public void push(int val) {
         stack.push(val);
         if (minStack.isEmpty() || val <= minStack.peek()) {
@@ -24,19 +22,18 @@ public class MinStack {
         }
     }
 
-    // pop from stack; if popped value equals top of minStack, pop minStack too
+
     public void pop() {
         if (stack.isEmpty()) {
             throw new NoSuchElementException("Stack is empty");
         }
         int popped = stack.pop();
-        // compare primitives to avoid Integer reference issues
         if (!minStack.isEmpty() && popped == minStack.peek()) {
             minStack.pop();
         }
     }
 
-    // return top element
+
     public int top() {
         if (stack.isEmpty()) {
             throw new NoSuchElementException("Stack is empty");
@@ -44,7 +41,7 @@ public class MinStack {
         return stack.peek();
     }
 
-    // return current minimum
+
     public int getMin() {
         if (minStack.isEmpty()) {
             throw new NoSuchElementException("Stack is empty");
@@ -52,7 +49,7 @@ public class MinStack {
         return minStack.peek();
     }
 
-    // simple demo
+
     public static void main(String[] args) {
         MinStack ms = new MinStack();
         ms.push(5);
@@ -60,20 +57,19 @@ public class MinStack {
         ms.push(2);
         ms.push(3);
 
-        System.out.println("Top: " + ms.top());       // 3
-        System.out.println("Min: " + ms.getMin());    // 2
+        System.out.println("Top: " + ms.top());
+        System.out.println("Min: " + ms.getMin());
 
-        ms.pop(); // pop 3
-        System.out.println("Top: " + ms.top());       // 2
-        System.out.println("Min: " + ms.getMin());    // 2
+        ms.pop();
+        System.out.println("Top: " + ms.top());
+        System.out.println("Min: " + ms.getMin());
 
-        ms.pop(); // pop 2 (one of the 2s)
-        System.out.println("Top: " + ms.top());       // 2
-        System.out.println("Min: " + ms.getMin());    // 2
+        ms.pop();
+        System.out.println("Top: " + ms.top());
+        System.out.println("Min: " + ms.getMin());
 
-        ms.pop(); // pop last 2
-        System.out.println("Top: " + ms.top());       // 5
-        System.out.println("Min: " + ms.getMin());    // 5
+        ms.pop();
+        System.out.println("Top: " + ms.top());
+        System.out.println("Min: " + ms.getMin());
     }
 }
-
