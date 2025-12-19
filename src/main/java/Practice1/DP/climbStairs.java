9d2048944e80602f6aa2f30e30e94e28108f5387
ishan.garg <officialgarg1@gmail.com>
2025-12-19 20:20:47 +0530

    done

--- src/main/java/Practice1/DP/climbStairs.java
+++ src/main/java/Practice1/DP/climbStairs.java
@@ -1,4 +1,5 @@
 package Practice1.DP;
+
 // https://www.youtube.com/watch?v=6OjGE04Kx_M&t=46s
 public class climbStairs {
     public int climbStairs(int n) {
@@ -27,12 +28,52 @@ class Solution4 {
 
     private static int rec(int n, int[] memo) {
         if (n < 0) return 0;
-        if(n==0) return 1;
+        if (n == 0) return 1;
         if (memo[n] > 0) return memo[n];
         int op1 = rec(n - 1, memo);
         int op2 = rec(n - 2, memo);
         memo[n] = op1 + op2;
         return op1 + op2;
+    }
+
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    int[] cost;
+
+    private int solve(int idx) {
+        // Base case
+        if (idx >= cost.length) {
+            return 0;
+        }
+
+        int a = cost[idx] + solve(idx + 1);
+        int b = cost[idx] + solve(idx + 2);
+
+        return Math.min(a, b);
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        this.cost = cost;
+        return Math.min(solve(0), solve(1));
+    }
+
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public int minCostClimbingStairsBottomUp(int[] cost) {
+        int n = cost.length;
+
+        // Edge case
+        if (n == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+
+        // Build DP in-place
+        for (int i = 2; i < n; i++) {
+            cost[i] = cost[i] + Math.min(cost[i - 1], cost[i - 2]);
+        }
+
+        // You can reach the top from either of last two steps
+        return Math.min(cost[n - 1], cost[n - 2]);
     }
 
 }
