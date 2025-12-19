4bd1ad83537858a9bc0ddfc861da73bb0260d0ac
ishan.garg <officialgarg1@gmail.com>
2025-12-19 19:08:38 +0530

    done

--- src/main/java/Practice1/DP/RobberHouse.java
+++ src/main/java/Practice1/DP/RobberHouse.java
@@ -1,4 +1,56 @@
 package Practice1.DP;
 
+import java.util.Arrays;
+
 public class RobberHouse {
+
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return robb(nums, nums.length - 1, dp);
+    }
+
+    public int robb(int[] nums, int index, int[] dp) {
+        // If there is only one house
+        if (index == 0) return nums[index];
+
+        // If there are no houses
+        if (index < 0) return 0;
+
+        // If already calculated, return stored value
+        if (dp[index] != -1) return dp[index];
+
+        // Option 1: Rob this house
+        int pick = nums[index] + robb(nums, index - 2, dp);
+
+        // Option 2: Skip this house
+        int notPick = robb(nums, index - 1, dp);
+
+        // Store and return the best option
+        return dp[index] = Math.max(pick, notPick);
+    }
+
 }
+
+/*
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];
+    }
+}
+ */
