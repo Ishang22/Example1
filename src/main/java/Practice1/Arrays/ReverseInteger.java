808f0421edcb3e31e39c309251930772a61bdab0
ishan.garg <officialgarg1@gmail.com>
2025-12-20 17:30:10 +0530

    done

--- src/main/java/Practice1/Arrays/ReverseInteger.java
+++ src/main/java/Practice1/Arrays/ReverseInteger.java
@@ -1,4 +1,15 @@
 package Practice1.Arrays;
 
 public class ReverseInteger {
+    static int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            rev = (rev * 10) + digit;
+            x = x / 10;
+        }
+
+        return rev;
+    }
 }
