94830c7c096253c894674518a4eb3dd47ea3a78b
ishan.garg <officialgarg1@gmail.com>
2025-12-31 17:28:17 +0530

    done

--- src/main/java/Practice1/Kadane/MaximumSumCircularSubarray.java
+++ src/main/java/Practice1/Kadane/MaximumSumCircularSubarray.java
@@ -2,56 +2,27 @@ package Practice1.Kadane;
 
 public class MaximumSumCircularSubarray {
 
-    // Kadane's Algorithm for Maximum Subarray Sum
-    private int kadanesMax(int[] nums) {
-        int sum = nums[0];
-        int maxSum = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(sum + nums[i], nums[i]);
-            maxSum = Math.max(maxSum, sum);
+    private int kadanes(int[] a){
+        int n = a.length;
+        int sum = a[0], answer = a[0];
+        for(int i=1; i<a.length; i++){
+            sum += a[i];
+            if(sum < a[i]) sum = a[i];
+            answer = Math.max(answer, sum);
         }
-
-        return maxSum;
+        return answer;
     }
-
-    // Kadane's Algorithm for Minimum Subarray Sum
-    private int kadanesMin(int[] nums) {
-        int sum = nums[0];
-        int minSum = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            sum = Math.min(sum + nums[i], nums[i]);
-            minSum = Math.min(minSum, sum);
+    public int maxSubarraySumCircular(int[] A) {
+        if(A.length == 0) return 0;
+        int x = kadanes(A);
+        int y = 0;
+        for(int i=0; i<A.length;i++){
+            y += A[i];
+            A[i] *= -1;
         }
-
-        return minSum;
-    }
-
-    public int maxSubarraySumCircular(int[] nums) {
-        int n = nums.length;
-
-        // 1. Total sum of array
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-
-        // 2. Maximum subarray sum (normal case)
-        int maxSum = kadanesMax(nums);
-
-        // 3. Minimum subarray sum
-        int minSum = kadanesMin(nums);
-
-        // 4. Circular subarray sum
-        int circularSum = totalSum - minSum;
-
-        // If all numbers are negative, circularSum becomes 0 (invalid)
-        if (maxSum > 0) {
-            return Math.max(maxSum, circularSum);
-        }
-
-        return maxSum;
+        int z = kadanes(A);
+        if(y+z == 0) return x;
+        return  Math.max(x, y+z);
     }
 
 }
--- src/main/java/Practice1/Kadane/MaximumSumSubArray.java
+++ src/main/java/Practice1/Kadane/MaximumSumSubArray.java
@@ -2,18 +2,15 @@ package Practice1.Kadane;
 
 public class MaximumSumSubArray {
 
-    public int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
-        int currmax = nums[0];
-
-        for(int i=1;i<nums.length;i++)
-        {
-            currmax= Math.max(nums[i],currmax+nums[i]);
-
-            maxSoFar=Math.max(maxSoFar,currmax);
+    public int maxSubArray(int[] a) {
+        int n = a.length;
+        int sum = a[0], answer = a[0];
+        for(int i=1; i<a.length; i++){
+            sum += a[i];
+            if(sum < a[i]) sum = a[i];
+            answer = Math.max(answer, sum);
         }
-
-        return maxSoFar;
+        return answer;
     }
 
 }
